e24517482e8cebccebb774e692388c2ab8458655
Dmitriy Kisil <logart1995@gmail.com>
2024-01-10 17:54:37 +0100

    Change order of options in dropdown

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ st.title("Image to text frontend app")
 
 prompt = st.text_input("Provide prompt")
 
-seed = st.sidebar.slider(label='seed', min_value=1, max_value=100, value=42, step=1)
-num_inference_steps = st.sidebar.slider(label='number of inference steps', min_value=1, max_value=100, value=50, step=1)
-guidance_scale = st.sidebar.slider(label='guidance scale', min_value=0.1, max_value=0.1, value=7.5, step=15.0)
 temperature = st.sidebar.slider(label='temperature', min_value=0.1, max_value=5.0, value=1.0, step=0.1)
+guidance_scale = st.sidebar.slider(label='guidance scale', min_value=0.1, max_value=0.1, value=7.5, step=15.0)
+num_inference_steps = st.sidebar.slider(label='number of inference steps', min_value=1, max_value=100, value=50, step=1)
+seed = st.sidebar.slider(label='seed', min_value=1, max_value=100, value=42, step=1)
 
 press_button = st.button('Generate image')
 if press_button:
